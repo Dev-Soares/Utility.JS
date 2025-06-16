f0cca5df4761eaaa79b710c872941761498036b4
Bernardo Soares <bernardo.soares30@outlook.com>
2025-06-15 21:37:21 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './assets/css/style.css';
 import { criaBotoes, criaItem } from './modules/createEl';
 
 
-
+//teste 
+//teste
+//teste
 const btnTarefa = document.querySelector('.btn-tarefa');
 
 btnTarefa.addEventListener('click', (e) => {
