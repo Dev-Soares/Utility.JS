d5c607e3ef90416601e0499f6fad0a0be525ca02
Bernardo Soares <bernardo.soares30@outlook.com>
2025-06-19 16:35:16 -0300

    pronto para deploy, img corrigida

--- src/main.js
+++ src/main.js
@@ -68,4 +68,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     if (path.endsWith('Lista.html')) {
       iniciarTasklist();
     }
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.menu-nav');
+
+    menuToggle?.addEventListener('click', () => {
+    nav.classList.toggle('open');
 });
+});
+
